ce3010912f2383cba677a2bb127cafdf35348ef1
geomer198 <geomer198@gmail.com>
2022-06-16 14:50:30 +0300

    [IMP] Add base auth. fix template.

--- lib/github/http.ex
+++ lib/github/http.ex
@@ -6,12 +6,20 @@ defmodule GitHub.HTTP do
     "https://api.github.com" <> url
   end
 
-  def process_request_header(headers) do
-    headers
+  def process_request_header(options) do
+    #    basic_auth = {"geomer198", "password"}
+    #    Keyword.update(
+    #      options,
+    #      :hackney,
+    #      Keyword.new(basic_auth: basic_auth),
+    #      &Keyword.put_new(&1, :basic_auth, basic_auth)
+    #    )
+    options
   end
 
   def process_response_body(body) do
-    body |> Poison.decode!
+    body
+    |> Poison.decode!
   end
 
 end
